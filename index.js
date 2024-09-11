f91f6af2127c46cb39418cc9e25e6ea03a8df372
Drew <drew.e.allred@gmail.com>
2024-09-11 15:05:02 -0400

    burger menu complete

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let menu = document.getElementsByClassName()
+
+
+function openMenu() {
+
+    document.body.classList += " menu--open";
+
+}
+
+function closeMenu() {
+
+    document.body.classList.remove( "menu--open" );
+
+}
